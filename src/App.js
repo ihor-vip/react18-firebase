93c5ed2fc963161e696b01a395d46e147493f283
ihor-vip <vip20140902@gmail.com>
2023-01-24 19:00:29 +0200

    UseEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
-import {planets} from "./planets";
+import { useState } from "react";
 
 function App() {
+    const [count, setCount] = useState(0);
+
+    const increase = () => {
+        setCount(count + 1);
+    };
+    const decrease = () => {
+        setCount(count - 1);
+    };
+    const setToZero = () => {
+        setCount(0);
+    };
 
     return (
         <div className="App">
-            {planets.map(
-                (planet, key) => !planet.isGasPlanet && <h1 key={planet.id}> {planet.name}  </h1>
-            )}
+            <button onClick={increase}>Increase</button>
+            <button onClick={decrease}>Decrease</button>
+            <button onClick={setToZero}>Set to Zero</button>
+
+            {count}
         </div>
     );
 }
