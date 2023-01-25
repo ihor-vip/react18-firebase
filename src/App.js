2ede1b3107f1cac22f5170aa77e587accea70fd7
ihor-vip <vip20140902@gmail.com>
2023-01-25 17:21:36 +0200

    Custom Hooks (reusable hooks)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
-import { Form } from "./components/Form";
+import {useToggle} from "./useToggle";
 
 function App() {
+    const [isVisible, toggle] = useToggle();
+    const [isVisible2, toggle2] = useToggle();
+
     return (
         <div className="App">
-            <Form />
+            <button onClick={toggle}>{isVisible ? "Hide_1" : "Show_1"}</button>
+            {isVisible && <h1> Hidden text </h1>}
+
+            <button onClick={toggle2}>{isVisible2 ? "Hide_2" : "Show_2"}</button>
+            {isVisible2 && <h1> Hidden text </h1>}
         </div>
     );
 }
